c0ef7f946c3c3054296d38d03d1439b152bb2e19
Levent Erkok <erkokl@gmail.com>
2015-11-08 22:24:34 -0800

    Further plumbing

--- src/Data/SBV/SBVPlugin.hs
+++ src/Data/SBV/SBVPlugin.hs
@@ -17,9 +17,26 @@ install _opts todos = do reinitializeGlobals
                                             return guts
 
 analyzeBind :: CoreBind -> CoreM ()
-analyzeBind (NonRec _ expr) = analyzeExpr expr
-analyzeBind (Rec binds)     = mapM_ (analyzeExpr . snd) binds
+analyzeBind = bind
+  where bind (NonRec _ e)    = expr e
+        bind (Rec binds)     = mapM_ (expr . snd) binds
 
-analyzeExpr :: CoreExpr ->  CoreM ()
-analyzeExpr e = do df <- getDynFlags
-                   liftIO $ putStrLn $ showSDoc df $ pprCoreExpr e
+        expr (Var{})         = return ()
+        expr (Lit{})         = return ()
+        expr (App f a)       = expr f >> expr a
+        expr l@(Lam{})       = analyze l
+        expr (Let b e)       = bind b >> expr e
+        expr (Case e _ _ as) = expr e >> mapM_ alt as
+        expr (Cast e _)      = expr e
+        expr (Tick _ e)      = expr e
+        expr (Type _)        = return ()
+        expr (Coercion _)    = return ()
+
+        alt (_, _, e)        = expr e
+
+        skipping e = do df <- getDynFlags
+                        liftIO $ putStrLn $ "SBVPlugin, skipping: " ++ showSDoc df (pprCoreExpr e)
+
+        analyze e@(Lam{} ) = do df <- getDynFlags
+                                liftIO $ putStrLn $ "SBVPlugin, considering: " ++ showSDoc df (pprCoreExpr e)
+        analyze e          = skipping e
