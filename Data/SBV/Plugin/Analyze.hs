321ece5b8702339fbf59642774af08d9f097fa83
Levent Erkok <erkokl@gmail.com>
2015-11-16 23:24:24 -0800

    Start analyzing expressions. WIP.

--- Data/SBV/Plugin/Analyze.hs
+++ Data/SBV/Plugin/Analyze.hs
@@ -3,20 +3,20 @@
 module Data.SBV.Plugin.Analyze (prove) where
 
 import GhcPlugins
-import PprCore
 
 import qualified Data.Map as M
 
-import qualified Data.SBV           as S
+import qualified Data.SBV           as S hiding (proveWith)
 import qualified Data.SBV.Dynamic   as S
-import qualified Data.SBV.Internals as S
+
+import qualified Control.Exception as C
 
 import Data.SBV.Plugin.Data
 
 -- | Prove an SBVTheorem
 prove :: Config -> Var -> SrcSpan -> CoreExpr -> IO ()
 prove cfg b s e
-  | isProvable (exprType e) = checkTheorem cfg loc e
+  | isProvable (exprType e) = safely $ checkTheorem cfg loc e
   | True                    = error $ "SBVPlugin: " ++ loc ++ " does not have a provable type!"
   where loc = showSDoc (dflags cfg) $ if isGoodSrcSpan s then ppr s <> text ":" <> ppr b else ppr b
 
@@ -25,24 +25,38 @@ prove cfg b s e
 isProvable :: Type -> Bool
 isProvable _ = True
 
+safely :: IO () -> IO ()
+safely a = a `C.catch` bad
+  where bad :: C.SomeException -> IO ()
+        bad = print
+
 getBaseType :: Config -> Type -> Maybe S.Kind
 getBaseType Config{knownTCs} t = case splitTyConApp_maybe t of
                                    Just (tc, []) -> tc `lookup` knownTCs
                                    _             -> Nothing
 
+type Env = M.Map Var S.SVal
+
 checkTheorem :: Config -> String -> CoreExpr -> IO ()
-checkTheorem cfg l topExpr = do
-        mbR <- S.runSymbolic' (S.Proof (False, S.defaultSMTCfg)) $ go M.empty topExpr
-        case mbR of
-          (Nothing,     _) -> todo
-          (Just (_, r), _) -> do print r
-                                 todo
-  where sh = showSDoc (dflags cfg) . pprCoreExpr
-        go m e@(Var v) = case v `M.lookup` m of
-                           Just s  -> return $ Just (m, s)
-                           Nothing -> case getBaseType cfg (exprType e) of
-                                        Just k  -> do s <- S.svMkSymVar Nothing k (Just (sh e))
-                                                      return $ Just (M.insert v s m, s)
-                                        Nothing -> return Nothing
-        go _ _         = return Nothing
-        todo  = liftIO $ putStrLn $ l ++ ": " ++ sh topExpr
+checkTheorem cfg l topExpr = print =<< S.proveWith S.defaultSMTCfg (snd `fmap` go M.empty topExpr)
+  where tbd w e = error $ "[SBVPlugin:" ++ l ++  "] Skipping proof. " ++ w ++ ":\n" ++ unlines e
+
+        sh o = showSDoc (dflags cfg) (ppr o)
+
+        go :: Env -> CoreExpr -> S.Symbolic (Env, S.SVal)
+
+        go m e@(Var v)
+           | Just s <- v `M.lookup` m
+           = return (m, s)
+           | True
+           = tbd "Expression refers to non-local variable" [sh e]
+
+        go m e@(Lam b body)
+           | Just k <- getBaseType cfg (varType b)
+           = do s <- S.svMkSymVar Nothing k (Just (sh b))
+                go (M.insert b s m) body
+           | True
+           = tbd "Abstraction with a non-basic binder" [sh e]
+
+        go _ e
+          = tbd "Expression with too complicated structure" [sh e]
