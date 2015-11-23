ec6db959d0449eba785436d486415a3effe6dd6d
Levent Erkok <erkokl@gmail.com>
2015-11-22 18:47:34 -0800

    Basic test of theoremhood/falsity

---
+++ tests/T00.hs
@@ -0,0 +1,14 @@
+module T00 where
+
+import Data.Word
+import Data.Int
+
+import Data.SBV.Plugin
+
+{-# ANN f theorem #-}
+f :: Word8 -> Bool -> Bool
+f _ _ = True
+
+{-# ANN g theorem {options = [WarnIfFails]} #-}
+g :: Int8 -> Bool -> Bool
+g _x _y = False
