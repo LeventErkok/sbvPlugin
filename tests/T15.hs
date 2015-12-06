e1a91b4c93ac9061eee00a4d1ad9011c1bb73291
Levent Erkok <erkokl@gmail.com>
2015-12-05 18:50:02 -0800

    Add test-case for new types
    Fixes #2

---
+++ tests/T15.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -fplugin=Data.SBV.Plugin #-}
+
+module T15 where
+
+import Data.SBV.Plugin
+
+newtype Age = Age Int deriving Eq
+
+{-# ANN f theorem {options = [Verbose, Names ["age"]]} #-}
+f :: Age -> Bool
+f (Age i) = i == i
