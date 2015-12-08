ce104f0203f6862df4f9b08edef5d39f0f1d7514
Levent Erkok <erkokl@gmail.com>
2015-12-07 21:42:13 -0800

    Add a no-inline to avoid GHC's simplifier from kicking in

--- tests/T17.hs
+++ tests/T17.hs
@@ -7,6 +7,7 @@ import Data.SBV.Plugin
 {-# ANN g sbv {options = [Uninterpret]} #-}
 g :: Int -> Int
 g _x = 0
+{-# NOINLINE g #-}  -- Avoid GHC from inlinining g first.
 
 {-# ANN f theorem #-}
 f :: Int -> Bool
