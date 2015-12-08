548e3fe151bd9d5a0e6840dcb478f118716bad14
Levent Erkok <erkokl@gmail.com>
2015-12-07 21:41:33 -0800

    Don't go verbose on these tests
    the binding names keep changing..

--- tests/T15.hs
+++ tests/T15.hs
@@ -6,6 +6,6 @@ import Data.SBV.Plugin
 
 newtype Age = Age Int deriving Eq
 
-{-# ANN f theorem {options = [Verbose, Names ["age"]]} #-}
+{-# ANN f theorem {options = [Names ["age"]]} #-}
 f :: Age -> Bool
 f (Age i) = i == i
