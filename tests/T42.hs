31b0a05c4bf84153d82203289fb15f689ade57a0
Levent Erkok <erkokl@gmail.com>
2015-12-21 14:44:58 -0800

    List test

---
+++ tests/T42.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -fplugin=Data.SBV.Plugin #-}
+
+module T42 where
+
+import Data.SBV.Plugin
+
+{-# ANN f theorem {options = [ListSize 5]} #-}
+f :: [Int] -> Bool
+f xs = s xs /= 10
+  where s []     = 0
+        s (x:xs) = x + s xs
+
+
+{-# ANN f ("HLint: ignore Use foldr" :: String) #-}
